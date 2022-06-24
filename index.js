e6e6bba821459937f285191a89ea1ce06510893c
Joschka Eickhoff <jeickhoff@posteo.de>
2022-06-24 11:31:11 +0200

    added auto-scroll-up to top

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * ONLY TO AUTO-SCROLL TO THE TOP ON RELOAD
+ */
+
 console.log("JS successfully connected to html-file!");
+console.log("");
+console.log("THIS IS ONLY TO AUTO-SCROLL TO THE TOP ON RELOAD");
+
+console.log("¯_(ツ)_/¯");
 
 window.scrollTo(0, 0);
